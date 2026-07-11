c7ea0b7f34af65202663ae5dc97f3c787c9d422d
Adeboye_Intern <oladimeji.adeboyeint@udc.edu>
2026-07-11 16:57:01 -0400

    Add admin-lambda-execution-role to Terraform (complete vulnerable lab)

--- infra/iam_roles.tf
+++ infra/iam_roles.tf
@@ -34,3 +34,24 @@ resource "aws_iam_role_policy" "low_priv_escalation_policy" {
     ]
   })
 }
+# The high-privilege role (the "prize")
+resource "aws_iam_role" "admin_lambda_execution_role" {
+  name = "admin-lambda-execution-role-tf"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach AdministratorAccess to the admin role
+resource "aws_iam_role_policy_attachment" "admin_policy_attach" {
+  role       = aws_iam_role.admin_lambda_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
